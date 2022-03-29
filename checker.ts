8904ea18c7ba0f99194280d19fd7045cf05d7f95
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2022-03-29 15:44:48 +0000

    cambio checker

--- checker.ts
+++ checker.ts
@@ -1,15 +1,10 @@
 function isPalindrome(text: string): boolean {
 console.log('isPalindrome/start');
 console.log(text);
-let j = text.length - 1;
-for (let i = 0; i < j; i++, j--) {
-    
-        i++;
-    if (i < j && text[i].toLowerCase() !== text[j].toLowerCase()) {
-        return false;
-    }
+const strReversed = text.split("").reverse().join("");
+  
+return strReversed === text ? true: false;
 }
-return true;
-};
+
 
 export default isPalindrome;
