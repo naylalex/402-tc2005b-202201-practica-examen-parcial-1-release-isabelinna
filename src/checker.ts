aacb8704711552b2017c1e27d9902eda4931f080
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2022-03-29 16:54:27 +0000

    cambio palindrome

--- src/checker.ts
+++ src/checker.ts
@@ -1,10 +1,18 @@
 function isPalindrome(text: string): boolean {
 console.log('isPalindrome/start');
 console.log(text);
-const strReversed = text.split("").reverse().join("");
-  
-return strReversed === text ? true: false;
+let middle : number = text.length/2;
+let bPalindrome : boolean = true;
+if(text.length % 2 != 0){
+  middle = middle - 0.5;
 }
-
-
+let iterator : number = text.length - 1;
+for(let i = 0; i < middle; i++){
+  if(text[i] != text[iterator]){
+    bPalindrome = false;
+  }
+  iterator-=1;
+}
+return bPalindrome;
+}
 export default isPalindrome;
