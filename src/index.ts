d982b14b4f0014d21bdce412db4da2d8e7fe0d53
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2022-03-29 15:57:01 +0000

    divResult

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import '/style.css';
-import isPalindrome from 'checker';
+import './style.css';
+import isPalindrome from './checker';
 console.log("index.ts/start");
 
-// obtener refencia denominada inputText
+//buttonsSubmitClick
+function buttonsSubmitClick(){
+//referencia inputtext
 const inputText =<HTMLInputElement>document.getElementById('inputText');
 
 //Invocar función isPalindrome
@@ -10,6 +12,14 @@ if(inputText){
 const bPalindrome = isPalindrome("inputText");
 console.log(bPalindrome);
 }
-
+}
 //Actualizar div result
 const divResult = <HTMLDivElement>document.getElementById('divResult');
+if(divResult){
+    if(isPalindrome("inputText")){
+        divResult.innerText = 'Es palindrome!!!';
+    }
+    else{
+        divResult.innerText = 'No es palindrome :c';
+    }
+}
