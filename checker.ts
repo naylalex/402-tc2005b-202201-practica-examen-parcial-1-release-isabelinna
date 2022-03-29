9538f26c8d48ec9dcc525bf8938da20b5fe411c3
Isabelinna <38509593+isabelinna@users.noreply.github.com>
2022-03-29 15:44:34 +0000

    cambio función palíndromo

--- checker.ts
+++ checker.ts
@@ -1,7 +1,15 @@
 function isPalindrome(text: string): boolean {
 console.log('isPalindrome/start');
 console.log(text);
-return false;
-
+let j = text.length - 1;
+for (let i = 0; i < j; i++, j--) {
+    
+        i++;
+    if (i < j && text[i].toLowerCase() !== text[j].toLowerCase()) {
+        return false;
+    }
 }
+return true;
+};
+
 export default isPalindrome;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import '/style.css';
 import isPalindrome from 'checker';
 console.log("index.ts/start");
+
+// obtener refencia denominada inputText
+const inputText =<HTMLInputElement>document.getElementById('inputText');
+
+//Invocar función isPalindrome
+if(inputText){
+const bPalindrome = isPalindrome("inputText");
+console.log(bPalindrome);
+}
+
+//Actualizar div result
+const divResult = <HTMLDivElement>document.getElementById('divResult');
